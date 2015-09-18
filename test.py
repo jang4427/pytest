2e7757269f8071e56c1b62cac60f0a116aedf1ac
JANGHEEJEONG <01692204427@naver.com>
2015-09-19 05:11:31 +0900

    Fix indentation

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ from blog.views import PostDetail, PostList, PostCreate
 
 class Test(TestCase):
     def test_1(self):  # read
-		detail = PostDetail()
+        detail = PostDetail()
 		#assert(False)
     def test_2(self):  # write
-		list = PostList()
+        list = PostList()
         # self.test_1()
     def test_3(self):
-		create = PostCreate()	
+        create = PostCreate()	
 		# assert 0
         # self.test_1()
